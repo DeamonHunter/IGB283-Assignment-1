087edaa4beb65ae680cc517b01db0496b67ededb
Deamonhunter <deamonhunterextra@outlook.com>
2017-10-08 17:37:44 +1000

    Implement Arm to specs. Clean up some unused things.

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -29,7 +29,8 @@ public class Controller : MonoBehaviour {
 
 
     private Shape Head;
-    private Shape Arm;
+    private Shape UpperArm;
+    private Shape LowerArm;
     private Shape Body;
 
     /// <summary>
@@ -42,26 +43,40 @@ public class Controller : MonoBehaviour {
 
         shapes = new List<Shape>();
 
-        Body = new Square(new Vector3(0.5f, 0.5f, 0), new Vector3(-0.5f, 0.5f, 0), new Vector3(-0.5f, -0.5f, 0), new Vector3(0.5f, -0.5f, 0));
+        Body = new Square(new Vector3(-1f, -1f, 0), new Vector3(-1f, 0f, 0), new Vector3(1f, 0f, 0), new Vector3(1, -1, 0));
+        Body.RotateCenter = new Vector3(0, -0.5f, 0);
         shapes.Add(Body);
-        Arm = new Square(new Vector3(-0.3f, 0.45f, 0), new Vector3(0.3f, 0.45f, 0), new Vector3(0.45f, 2.4f, 0), new Vector3(-0.45f, 2.4f, 0));
-        Arm.RotateCenter = new Vector3(0, 0.45f, 0);
-        Body.AddChild(Arm);
-        shapes.Add(Arm);
-        Head = new Square(new Vector3(-0.1f, 2.3f, 0), new Vector3(0.1f, 2.3f, 0), new Vector3(0.1f, 2.8f, 0), new Vector3(-0.1f, 2.8f, 0));
-        Head.RotateCenter = new Vector3(0, 2.3f, 0);
-        Arm.AddChild(Head);
+
+        UpperArm = new Square(new Vector3(-0.25f, 0f, 0), new Vector3(-0.25f, 2f, 0), new Vector3(0.25f, 2f, 0), new Vector3(0.25f, 0f, 0));
+        UpperArm.RotateCenter = new Vector3(0, 0f, 0);
+        Body.AddChild(UpperArm);
+        shapes.Add(UpperArm);
+
+        LowerArm = new Square(new Vector3(-0.25f, 2f, 0), new Vector3(-0.25f, 4f, 0), new Vector3(0.25f, 4f, 0), new Vector3(0.25f, 2f, 0));
+        LowerArm.RotateCenter = new Vector3(0, 2f, 0);
+        UpperArm.AddChild(LowerArm);
+        shapes.Add(LowerArm);
+
+        Head = new Square(new Vector3(-0.1f, 4f, 0), new Vector3(-0.1f, 5f, 0), new Vector3(0.1f, 5f, 0), new Vector3(0.1f, 4f, 0));
+        Head.RotateCenter = new Vector3(0, 4f, 0);
+        LowerArm.AddChild(Head);
         shapes.Add(Head);
     }
 
 
     // Update is called once per frame
     private void Update() {
-        IGB283.Matrix3x3 T = IGB283Transform.Translate(-(Vector2)Arm.RotateCenter);
+        IGB283.Matrix3x3 T = IGB283Transform.Translate(-(Vector2)UpperArm.RotateCenter);
         IGB283.Matrix3x3 R = IGB283Transform.Rotate(50f * Time.deltaTime);
-        IGB283.Matrix3x3 TReverse = IGB283Transform.Translate((Vector2)Arm.RotateCenter);
+        IGB283.Matrix3x3 TReverse = IGB283Transform.Translate((Vector2)UpperArm.RotateCenter);
 
-        Arm.ApplyTransformation(TReverse * R * T);
+        UpperArm.ApplyTransformation(TReverse * R * T);
+
+        T = IGB283Transform.Translate(-(Vector2)LowerArm.RotateCenter);
+        R = IGB283Transform.Rotate(-50f * Time.deltaTime);
+        TReverse = IGB283Transform.Translate((Vector2)LowerArm.RotateCenter);
+
+        LowerArm.ApplyTransformation(TReverse * R * T);
 
 
         T = IGB283Transform.Translate(-(Vector2)Head.RotateCenter);
@@ -93,7 +108,7 @@ public class Controller : MonoBehaviour {
         mesh.vertices = points.ToArray();
         mesh.triangles = triangles.ToArray();
 
-        mesh.colors = new Color[] { Color.white, Color.white, Color.white, Color.white, Color.black, Color.black, Color.black, Color.black, Color.red, Color.red, Color.red, Color.red };
+        mesh.colors = new Color[] { Color.white, Color.white, Color.white, Color.white, Color.black, Color.black, Color.black, Color.black, Color.red, Color.red, Color.red, Color.red, Color.blue, Color.blue, Color.blue, Color.blue };
     }
 }
 
